b33fe295fe4b042385c2d73293d0743071fc08d3
SDILIPS <dilip.sridhar.c@gmail.com>
2019-01-10 15:36:40 +0100

    TwitterSentimentAnalysis.py

--- TwitterSentimentAnalysis.py
+++ TwitterSentimentAnalysis.py
@@ -1,11 +1,11 @@
 import tweepy 
 from textblob import TextBlob
 
-consumerKey = '4lPXzlNfC2ZP9oKLFL0R2eQP8'
-consumerSecret = 'HPktFzlVHnu3ppSYBwZFcwPnr8b0tp25StjjIjskeVPyykiaBK'
+consumerKey = 'XYZ'
+consumerSecret = 'XYZ'
 
-accessToken = '76032279-HREDBimLDIHOmBvaKO0ZLgzaJNLRPj9UIcEjWIvS8'
-accessTokenSecret = 'bXgeAvB21S6AgG4mnWga7XXau7ylsVRQ89NwJeKJlTkv8'
+accessToken = 'XYZ'
+accessTokenSecret = 'XYZ'
 
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
